fee7e4bd39c32884d027eb8426d216d1451297ba
FinagleLord <72840971+FinagleLord@users.noreply.github.com>
2021-04-09 04:44:12 -0500

    Update MasterChef_Timelock.sol

--- contracts/MasterChef_Timelock.sol
+++ contracts/MasterChef_Timelock.sol
@@ -4,7 +4,16 @@
 
 pragma solidity ^0.6.12;
 
-import "../../libraries/IERC20.sol";
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 interface IChef {
     function add(uint256 _allocPoint,IERC20 _lpToken,bool _withUpdate) public;
